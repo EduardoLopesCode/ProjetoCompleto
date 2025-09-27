5c3296e4a2a5f1f1dce084ac5dfb074da2b158b6
Eduardo Antônio Lopes da Silveira <ealopzprodev@gmail.com>
2025-09-27 02:38:43 +0000

    `Projeto Pessoal`

--- Lógica/funcao.js
+++ Lógica/funcao.js
@@ -1,7 +1,8 @@
-function MaiorNumero(a, b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
+function Dirigir(idade, carteira) {
+   
+  if (idade >= 18 && carteira === true) {
+    console.log("Está Apto a Dirigir!");
+  } else {
+    console.log("Ainda Não Pode Dirigir!");
+  }
 }
--- Lógica/laco.js
+++ Lógica/laco.js
@@ -1,4 +1,9 @@
-var i = 1
-for (var i = 1; i <= 10; i++) {
-    console.log(n, "x", i, "-", n * i);
-}
+let i = 1 
+for (let i = 1; i<=100; i++){
+    if ( i % 2 ){
+        console.log("É Impar")
+    } else {
+        console.log("É Par!")
+    }
+    console.log(i)
+} 
